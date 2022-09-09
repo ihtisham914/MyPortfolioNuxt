787c99be574c421ca6f9863d6353f8a1e695e99a
Ihtisham Ul haq <you@example.com>
2022-09-09 17:11:23 +0500

    links to images changed

--- components/ProjectSection.vue
+++ components/ProjectSection.vue
@@ -23,7 +23,7 @@ export default {
       This is first of my projects."
       link="https://chatresponsivedesign.netlify.app"
       code="https://github.com/ihtisham914/chatApplication.git"
-      img_1="chat.webp"
+      img_1="../assets/images/chat.webp"
     />
 
     <Project
@@ -31,14 +31,14 @@ export default {
       para="CharsadwalChappal is an E-commerce website built for a client. It is a full stack website. I designed it and built it. you can buy Traditional Charsadwal Chappal from here as well as new trending designs."
       link="https://charsadwalchappal.000webhostapp.com/client/index.php"
       code="https://github.com/ihtisham914/charsadwalChappalphp.git"
-      img_1="chappal.webp"
+      img_1="../assets/images/chappal.webp"
     />
     <Project
       title="CodingTech => built in React:"
       para="CodingTech is a simple, responsive Front-end website that is built in React. A customer can visit this site to get online services for web development. He can view profiles of the Team as well. He can contact the vendors through a form."
       link="https://codingtechreact.netlify.app"
       code="https://github.com/ihtisham914/CodingTechReact.git"
-      img_1="Codingtech1.webp"
+      img_1="../assets/images/Codingtech1.webp"
     />
 
     <Project
@@ -46,7 +46,7 @@ export default {
       para="My own reponsive portfolio built in vue.js and Nuxt.js. The Desing is inspired from the new trending design 'Glassmorphism'. Here I have listed all my skills. I showcased all the projects that I have been working on."
       link="https://ihtishamportfolio.netlify.app"
       code="https://github.com/ihtisham914/myportfolionuxt.git"
-      img_1="portfolio.webp"
+      img_1="../assets/images/portfolio.webp"
     />
   </div>
 </template>
